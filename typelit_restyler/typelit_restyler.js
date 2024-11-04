617d56eaf96450a88f90e0e3e6290ac90d75aa66
frogssoldseparately <frogssoldseparately@gmail.com>
2024-11-04 01:00:53 -0500

    Fixed menu bug, minor refactor
    Fixed a bug where clicking the settings cog too soon after closing the settings would result in the additions to the theme menu to no longer appear.
    
    Cleaned up the settingsButtonAction function to improve readability marginally.
    
    Added a maximum amount of checks the waitForElement function can do before failing.
    
    Sprinkled in comments.

--- typelit_restyler/typelit_restyler.js
+++ typelit_restyler/typelit_restyler.js
@@ -16,17 +16,113 @@
     'use strict';
     // checks to see if elemFetchHandle returns a non-null value in a set interval.
     // once it returns a non-null value, runs actionHandle with that value as an input.
-    function waitForElement(elemFetchHandle, actionHandle, interval) {
+    function waitForElement(elemFetchHandle, actionHandle, interval, name="element") {
+        let iterationMax = 100; // maximum amount of allowed checks. Throws an error after this many attempts
         let intervalId = setInterval(() => {
-            const elem = elemFetchHandle();
-            if (!elem) {
-                console.log(`Waiting for element to load. Checking again in ${interval}ms.`);
-                return;
+            if (iterationMax === 0) {
+                clearInterval(intervalId);
+                throw new Error(`${name} didn't load. Maximum iterations surpassed.`);
             }
-            console.log('Element loaded.');
-            clearInterval(intervalId);
-            actionHandle(elem);
+            const elem = elemFetchHandle();
+            if (elem) {
+                console.log(`${name} has loaded.`);
+                clearInterval(intervalId);
+                actionHandle(elem);
+            } else {
+                console.log(`Waiting for ${name} to load. Checking again in ${interval}ms.`);
+                iterationMax--;
+            }
         }, interval);
+    }
+    // generates additional theme elements and appends them to the provided theme menu
+    function generateThemes(elem) {
+        // fetching necessary classnames
+        const headerClass = elem.querySelector('div[class*="headerMargin"]').className;
+        const separatorClass = elem.querySelector('hr[class*="divider"]').className;
+        const themeButtonGroupExample = elem.querySelectorAll('div[class*="themeButtonSection"]')[1];
+        const themeContainerExamples = themeButtonGroupExample.querySelectorAll('div[class*="premiumThemeButtonContainer"]');
+
+        const firstThemeContainerExample = themeContainerExamples[0];
+        const themeContainerExample = themeContainerExamples[1];
+        const lastThemeContainerExample = themeContainerExamples[themeContainerExamples.length - 1];
+
+        const firstThemeButtonExample = firstThemeContainerExample.firstChild;
+        const themeButtonExample = themeContainerExample.firstChild;
+        const lastThemeButtonExample = lastThemeContainerExample.firstChild;
+
+        const themeButtonGroupClass = themeButtonGroupExample.className;
+        const themeContainerClass = themeContainerExample.className;
+        const firstThemeButtonClass = firstThemeButtonExample.className;
+        const themeButtonClass = themeButtonExample.className;
+        const lastThemeButtonClass = lastThemeButtonExample.className;
+
+        const colorChipClass = themeButtonExample.firstChild.className;
+        const labelClass = themeButtonExample.childNodes[1].className;
+        const spanClass = themeButtonExample.lastChild.className;
+
+        // creates horizontal rule to add space between premium themes and additional themes
+        const separator = document.createElement('hr');
+        separator.className = separatorClass;
+        elem.appendChild(separator);
+
+        // creates header for the additional themes button container
+        const newStyleHeader = document.createElement('div');
+        newStyleHeader.className = headerClass;
+        newStyleHeader.appendChild(document.createTextNode('Naughty Themes'));
+        elem.appendChild(newStyleHeader);
+
+        // creates a button section for the additional themes
+        const newButtonSection = document.createElement('div');
+        newButtonSection.className = themeButtonGroupClass;
+        newButtonSection.addEventListener("mouseleave", () => {
+            styleContainer.revertPick();
+        });
+        elem.appendChild(newButtonSection);
+
+        // creates a theme button for each additional style
+        const additionalStyles = styleContainer.styles;
+        for (var i = 0; i < additionalStyles.length; i++) {
+            const style = additionalStyles[i];
+            // creates a theme container
+            const buttonContainer = document.createElement('div');
+            buttonContainer.className = themeContainerClass;
+
+            // creates the button that does the work
+            const styleButton = document.createElement('button');
+            // ensure proper class assignment so the button corners round properly.
+            switch (i) {
+                case 0: styleButton.className = firstThemeButtonClass; break;
+                case additionalStyles.length - 1: styleButton.className = lastThemeButtonClass; break;
+                default: styleButton.className = themeButtonClass;
+            }
+            // styleButton.className = themeButtonClass;
+            styleButton.setAttribute('tabindex', 0);
+            styleButton.setAttribute('type', 'button');
+            styleButton.style.background = style.style.pageBackground;
+            styleButton.style.color = style.style.untypedColor;
+            styleButton.addEventListener('click', () => {
+                styleContainer.pickByName(style.name);
+                document.querySelector('div[role="presentation"]').firstChild.click();
+            });
+            styleButton.addEventListener('mouseenter', () => styleContainer.temporaryPickByName(style.name));
+
+            // create theme display elements for the button
+            const menuChip = document.createElement('div');
+            menuChip.className = colorChipClass;
+            menuChip.style.background = style.style.blinkBackground;
+            const label = document.createElement('div');
+            label.className = labelClass;
+            label.appendChild(document.createTextNode(style.name.toUpperCase()));
+            const span = document.createElement('span');
+            span.className = spanClass;
+
+            // append button elements to button, button to container, and container to button section
+            styleButton.appendChild(menuChip);
+            styleButton.appendChild(label);
+            styleButton.appendChild(span);
+            buttonContainer.appendChild(styleButton);
+            newButtonSection.appendChild(buttonContainer);
+        }
     }
     // add necessary listeners to settings elements to make custom themes appear
     function settingsButtonAction() {
@@ -41,109 +137,24 @@
             })[0];
         }, (elem) => {
             // readds the click listener to the settings button when settings are closed.
-            // this is necessary because the settings button gets replaced when the settings close.
+            // this is necessary because the settings button gets replaced.
             document.querySelector('div[role="presentation"]').firstChild.addEventListener('click', () => {
-                setTimeout(() => {
-                    waitForElement(() => document.querySelector('button[aria-label="Settings"]'), (elem) => {
-                        elem.addEventListener('click', settingsButtonAction);
-                    }, 100);
-                }, 200);
+                const heldInstance = document.querySelector('button[aria-label="Settings"]');
+                waitForElement(() => {
+                    // waits for the settings button to get replaced
+                    const checkedInstance = document.querySelector('button[aria-label="Settings"]');
+                    return checkedInstance !== heldInstance ? checkedInstance : null;
+                }, (elem) => {
+                    // adds the listener to the new button
+                    elem.addEventListener('click', settingsButtonAction);
+                }, 50, "Settings Button");
             });
             // generates additional theme buttons when the theme settings panel is opened
             elem.addEventListener('click', () => {
-                // returns immediately if the theme menu is already open
                 if (document.querySelector('div[class$="themeMenu"]')) return;
-                waitForElement(() => document.querySelector('div[class*="themeMenu"]'), (elem) => {
-                    // fetching necessary classnames
-                    const headerClass = elem.querySelector('div[class*="headerMargin"]').className;
-                    const separatorClass = elem.querySelector('hr[class*="divider"]').className;
-                    const themeButtonGroupExample = elem.querySelectorAll('div[class*="themeButtonSection"]')[1];
-                    const themeContainerExamples = themeButtonGroupExample.querySelectorAll('div[class*="premiumThemeButtonContainer"]');
-
-                    const firstThemeContainerExample = themeContainerExamples[0];
-                    const themeContainerExample = themeContainerExamples[1];
-                    const lastThemeContainerExample = themeContainerExamples[themeContainerExamples.length - 1];
-
-                    const firstThemeButtonExample = firstThemeContainerExample.firstChild;
-                    const themeButtonExample = themeContainerExample.firstChild;
-                    const lastThemeButtonExample = lastThemeContainerExample.firstChild;
-
-                    const themeButtonGroupClass = themeButtonGroupExample.className;
-                    const themeContainerClass = themeContainerExample.className;
-                    const firstThemeButtonClass = firstThemeButtonExample.className;
-                    const themeButtonClass = themeButtonExample.className;
-                    const lastThemeButtonClass = lastThemeButtonExample.className;
-
-                    const colorChipClass = themeButtonExample.firstChild.className;
-                    const labelClass = themeButtonExample.childNodes[1].className;
-                    const spanClass = themeButtonExample.lastChild.className;
-
-                    // creates horizontal rule to add space between premium themes and additional themes
-                    const separator = document.createElement('hr');
-                    separator.className = separatorClass;
-                    elem.appendChild(separator);
-
-                    // creates header for the additional themes button container
-                    const newStyleHeader = document.createElement('div');
-                    newStyleHeader.className = headerClass;
-                    newStyleHeader.appendChild(document.createTextNode('Naughty Themes'));
-                    elem.appendChild(newStyleHeader);
-
-                    // creates a button section for the additional themes
-                    const newButtonSection = document.createElement('div');
-                    newButtonSection.className = themeButtonGroupClass;
-                    newButtonSection.addEventListener("mouseleave", () => {
-                        styleContainer.revertPick();
-                    });
-                    elem.appendChild(newButtonSection);
-
-                    // creates a theme button for each additional style
-                    const additionalStyles = styleContainer.styles;
-                    for (var i = 0; i < additionalStyles.length; i++) {
-                        const style = additionalStyles[i];
-                        // creates a theme container
-                        const buttonContainer = document.createElement('div');
-                        buttonContainer.className = themeContainerClass;
-
-                        // creates the button that does the work
-                        const styleButton = document.createElement('button');
-                        // ensure proper class assignment so the button corners round properly.
-                        switch (i) {
-                            case 0: styleButton.className = firstThemeButtonClass; break;
-                            case additionalStyles.length - 1: styleButton.className = lastThemeButtonClass; break;
-                            default: styleButton.className = themeButtonClass;
-                        }
-                        // styleButton.className = themeButtonClass;
-                        styleButton.setAttribute('tabindex', 0);
-                        styleButton.setAttribute('type', 'button');
-                        styleButton.style.background = style.style.pageBackground;
-                        styleButton.style.color = style.style.untypedColor;
-                        styleButton.addEventListener('click', () => {
-                            styleContainer.pickByName(style.name);
-                            document.querySelector('div[role="presentation"]').firstChild.click();
-                        });
-                        styleButton.addEventListener('mouseenter', () => styleContainer.temporaryPickByName(style.name));
-
-                        // create theme display elements for the button
-                        const menuChip = document.createElement('div');
-                        menuChip.className = colorChipClass;
-                        menuChip.style.background = style.style.blinkBackground;
-                        const label = document.createElement('div');
-                        label.className = labelClass;
-                        label.appendChild(document.createTextNode(style.name.toUpperCase()));
-                        const span = document.createElement('span');
-                        span.className = spanClass;
-
-                        // append button elements to button, button to container, and container to button section
-                        styleButton.appendChild(menuChip);
-                        styleButton.appendChild(label);
-                        styleButton.appendChild(span);
-                        buttonContainer.appendChild(styleButton);
-                        newButtonSection.appendChild(buttonContainer);
-                    }
-                }, 50);
+                waitForElement(() => document.querySelector('div[class$="themeMenu"]'), generateThemes, 50, "Themes Menu");
             });
-        }, 100);
+        }, 50, "Themes Button");
     };
 
     const restylerLSName = "restyler-selected-style"; // localStorage field name for last selected theme
@@ -266,5 +277,5 @@
     // Makes additional themes appear in the theme selector
     waitForElement(() => document.querySelector('button[aria-label="Settings"]'), (elem) => {
         elem.addEventListener('click', settingsButtonAction);
-    }, 100);
+    }, 100, "Settings Button");
 })();
